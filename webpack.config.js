e04529014515763f32b4059d7a5af27039707d40
Fcc66 <1351686735@qq.com>
2017-06-07 16:40:36 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    path: path.resolve(__dirname, './output'),
+    publicPath: '/output/',
     filename: 'build.js'
   },
   module: {
